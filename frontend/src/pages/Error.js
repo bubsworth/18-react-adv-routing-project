0de9762adc530ee88104ad3c4f7b628357fa5f7e
bubsworth <bubbard96@hotmail.com>
2024-02-21 14:58:32 +0000

    Which Kind Of Code Goes Into loader()s?

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,13 +1,27 @@
-import MainNavigation from "../components/MainNavigation";
+import { useRouteError } from "react-router-dom";
+
+import PageContent from "../components/PageContent";
 
 function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occured!";
+  let message = "Could not find this page!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
   return (
     <>
-      <MainNavigation />
-      <main>
-        <h1>An error occured!</h1>
-        <p>Could not find this page!</p>
-      </main>
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
     </>
   );
 }
