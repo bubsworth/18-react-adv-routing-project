452823b03aa30954ff4c392ae6f2b7697a4d57d4
bubsworth <bubbard96@hotmail.com>
2024-02-22 10:15:36 +0000

    Validating User Input & Outputting Validation Errors

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -18,13 +18,17 @@ export async function action({ request, params }) {
     description: data.get("description"),
   };
 
-  const response = fetch("http://localhost:8080/events", {
+  const response = await fetch("http://localhost:8080/events", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(eventData),
   });
+
+  if (response.statusCode === 422) {
+    return response;
+  }
 
   if (!response.ok) {
     throw json({ message: "Could not save event." }, { status: 500 });
